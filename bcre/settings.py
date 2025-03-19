7f2fcc1841fc73ff52efd203731679c54b1572a8
aaronwai <aaron.lung.wai@gmail.com>
2025-03-19 16:34:38 +0800

    Step 14 fix sendmail

--- bcre/settings.py
+++ bcre/settings.py
@@ -147,9 +147,9 @@ MESSAGE_TAGS = {
     messages.ERROR : 'danger',
     messages.SUCCESS : 'success'
 }
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'aaron.lung.wai@gmail.com'
+EMAIL_HOST_PASSWORD = 'beociionvakqcqey'
 EMAIL_USE_TLS = True
